09b66d4efeb72ae38e918b0e3802b36338da3923
CyrilBaah <baah_yard12@yahoo.com>
2025-10-30 04:55:38 +0000

    Add IAM OIDC Resource

--- terraform/modules/github-oidc/outputs.tf
+++ terraform/modules/github-oidc/outputs.tf
@@ -1,9 +1,9 @@
 output "github_actions_role_arn" {
   description = "ARN of the GitHub Actions IAM role"
-  value       = data.aws_iam_role.github_actions.arn
+  value       = aws_iam_role.github_actions.arn
 }
 
 output "oidc_provider_arn" {
   description = "ARN of the GitHub OIDC provider"
-  value       = data.aws_iam_openid_connect_provider.github.arn
+  value       = aws_iam_openid_connect_provider.github.arn
 }
