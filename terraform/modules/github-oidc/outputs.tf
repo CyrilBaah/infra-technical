8ff44cc63e51ed3a9c66b0178e35aedc664649c2
CyrilBaah <baah_yard12@yahoo.com>
2025-10-30 02:24:41 +0000

    Fix existing entity

--- terraform/modules/github-oidc/outputs.tf
+++ terraform/modules/github-oidc/outputs.tf
@@ -1,6 +1,6 @@
 output "github_actions_role_arn" {
   description = "ARN of the GitHub Actions IAM role"
-  value       = aws_iam_role.github_actions.arn
+  value       = data.aws_iam_role.github_actions.arn
 }
 
 output "oidc_provider_arn" {
